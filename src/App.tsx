30956fedd428f4651ddf2cb9400675b537146e45
E-ppo <93433413+E-ppo@users.noreply.github.com>
2026-04-10 22:05:04 +0900

    feat: 라우트 설정
    검색, 찜 페이지 라우트 구성
    
    주요 변경사항
    - 라우트 상수 정의 (constants/routes.ts)
    - SearchPage, WishlistPage 페이지 컴포넌트 생성
    - App.tsx 라우트 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import Icon from "./components/Icon/Icon";
+import { ROUTES } from "@/constants/routes";
+import SearchPage from "@/pages/Search/SearchPage";
+import WishlistPage from "@/pages/Wishlist/WishlistPage";
 
 function App() {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          <div>
-            <Icon name="LikeFill" width={24} height={24} />
-            Home
-          </div>
-        }
-      />
+      <Route path={ROUTES.SEARCH} element={<SearchPage />} />
+      <Route path={ROUTES.WISHLIST} element={<WishlistPage />} />
     </Routes>
   );
 }
---
+++ src/pages/Wishlist/WishlistPage.tsx
@@ -0,0 +1,3 @@
+export default function WishlistPage() {
+  return <div>찜</div>
+}
